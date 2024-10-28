90e95fdcbe62793a26e40d2dcb9b6e22102a5140
Hexirost <hexirost@gmail.com>
2024-10-28 11:19:32 -0700

    Inital Commit

---
+++ InfiniteCompendium/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using InfiniteCompendium.Models;
+
+namespace InfiniteCompendium.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<InfiniteCompendium.Models.Books> Books { get; set; } = default!;
+    }
+}
---
+++ InfiniteCompendium/Models/Books.cs
@@ -0,0 +1,11 @@
+﻿namespace InfiniteCompendium.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public required string Title { get; set; }
+        public string? Author { get; set; }
+        public required bool status{ get; set; }
+        public string? Category {  get; set; }
+    }
+}
